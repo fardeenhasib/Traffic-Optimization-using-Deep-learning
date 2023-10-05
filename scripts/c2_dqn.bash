348581dc356d07c30b8d70b77fc86e74b8793d08
Fardeen Hasib Mozumder <fardeenmozumder@gmail.com>
2023-10-04 22:47:44 -0500

    Add files via upload

---
+++ scripts/c2_dqn.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file_name="log/dqn_jinan_3x4.log"
+
+info_file="agent/configs_new_dqn6/"
+dataset_name="envs/jinan_3x4/config_dqn.json"
+
+min_test=1
+max_test=30
+
+echo "...Starting all sdqn..." | tee $file_name
+for i in $(seq $min_test $max_test)
+do
+   echo "Running $i/$max_test, default.json"  | tee -a $file_name
+   python run_new_dqn_phase.py $dataset_name --parameters $info_file"default2.json" | tee -a $file_name
+   printf "\n" | tee -a $file_name
+done   
+
+python visualization/view-all.py "log/my_new_dqn/" | tee -a $file_name
+
---
+++ scripts/run_all_new_dqn.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+file_name="log/dqn_jinan_3x4_all.log"
+
+info_file="agent/configs_new_dqn5/"
+dataset_name="envs/jinan_3x4/config_dqn.json"
+
+min_test=0
+max_test=14
+
+echo "...Starting all new_dqn..." | tee $file_name
+for i in $(seq $min_test $max_test)
+do
+   echo "Running $i/$max_test, $info_file$i.json"  | tee -a $file_name
+   python run_new_dqn_phase.py $dataset_name --parameters $info_file$i".json" | tee -a $file_name
+   printf "\n" | tee -a $file_name
+done   
+
+python visualization/view-all.py "log/my_new_dqn/" | tee -a $file_name
+
